8af0e6f0f45bafbf8ce27a84aacb86d84901240c
Prakash Shanbhag <praks.1529@gmail.com>
2023-10-13 21:08:43 +0530

    Add home console

---
+++ app/(console)/home/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React, { useState, FormEvent } from 'react'
+
+export default function Landing() {
+    return (
+        <section>
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
+                <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
+                    <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+                        <h1 className="h1 mb-4" data-aos="fade-up">
+                            Landing
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
