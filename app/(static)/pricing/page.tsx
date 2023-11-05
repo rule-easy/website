bde1d7bd610994b52fc3cbeb29783d608a68a9ad
Prakash Shanbhag <praks.1529@gmail.com>
2023-11-05 21:58:02 +0530

    Update pricing page

--- app/(static)/pricing/page.tsx
+++ app/(static)/pricing/page.tsx
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 import {
-    faArrowRight, faBan, faBoltLightning, faCheckCircle, faCircleXmark
+    faArrowRight, faBan, faBoltLightning, faCheckCircle, faCircleXmark, faEnvelope, faPhone
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -58,55 +58,10 @@ export default function Pricing() {
                                     <button type="submit" className="w-full flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-indigo-100 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Get pricing <FontAwesomeIcon icon={faArrowRight} className="pl-4 text-indigo-100" /></button>
                                 </div>
                             </div>
-                            {/* Table row-1 */}
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3 "> ML playground</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
-                            </div>
-                            {/* Table row-2 */}
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3"> Access to models</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Basic</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Advanced</span>
-                            </div>
-                            {/* Table row-3 */}
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">ML rules</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
-                            </div>
-                            {/* Table row-4 */}
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Total ML workflows</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">5 per week</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Unlimited</span>
-                            </div>
-                            {/* Table row-4 */}
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">ML data size</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 50 MB</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 1GB</span>
-                            </div>
 
                             {/* Core platform features */}
                             <div className="grid grid-cols-1">
                                 <span className="inline-block text-center font-bold mt-3 mb-3 text-indigo-600">Core platform features</span>
-                            </div>
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Historical analysis</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCircleXmark} size="xl" className="text-custom-red" /></span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
-                            </div>
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Data retention</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">7 days</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">60 days</span>
-                            </div>
-                            <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Webhook support</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCircleXmark} size="xl" className="text-custom-red" /></span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
                             </div>
                             <div className="grid grid-cols-3 text-left">
                                 <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Number of rules</span>
@@ -115,16 +70,26 @@ export default function Pricing() {
                             </div>
                             <div className="grid grid-cols-3 text-left">
                                 <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Free rule evaluation</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 10K requests</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 25K requests</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 10K requests / month</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 25K requests / month</span>
                             </div>
                             <div className="grid grid-cols-3">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">On-demand rule evaluation</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">$30 per 10K req</span>
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Paid rule evaluation</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">$30 per 10K requests</span>
                                 {/* <button type="submit" className="w-2/3 flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ">On request <FontAwesomeIcon icon={faArrowRight} color="#FFF" className="pl-4" /></button> */}
                             </div>
                             <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">24X7 support</span>
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Historical analysis</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCircleXmark} size="xl" className="text-custom-red" /></span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Data retention</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">3 days</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">60 days</span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Webhook support</span>
                                 <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCircleXmark} size="xl" className="text-custom-red" /></span>
                                 <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
                             </div>
@@ -133,6 +98,46 @@ export default function Pricing() {
                                 <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCircleXmark} size="xl" className="text-custom-red" /></span>
                                 <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
                             </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Support</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faEnvelope} size="xl" className="text-custom-green" /></span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faPhone} size="xl" className="text-custom-green" /></span>
+                            </div>
+
+                            {/* Advanced features */}
+                            <div className="grid grid-cols-1">
+                                <span className="inline-block text-center font-bold mt-3 mb-3 text-indigo-600">Advanced features</span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3 "> ML playground</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3"> Supported ML models</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Basic</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Advanced</span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">ML rules</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Total ML workflows</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">5 per week</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Unlimited</span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">ML training data size</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 50 MB</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">Upto 1GB</span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Max number of features</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">10</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">100</span>
+                            </div>
 
                             {/* Console features */}
                             <div className="grid grid-cols-1">
@@ -140,14 +145,19 @@ export default function Pricing() {
                             </div>
                             {/* Table row-6 */}
                             <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Number of admin users</span>
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Core features</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                            </div>
+                            <div className="grid grid-cols-3 text-left">
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Total free users</span>
                                 <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">1</span>
                                 <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">5</span>
                             </div>
                             <div className="grid grid-cols-3 text-left">
-                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Team support</span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCircleXmark} size="xl" className="text-custom-red" /></span>
-                                <span className="inline-block text-center font-semibold mt-3 mb-3"><FontAwesomeIcon icon={faCheckCircle} size="xl" className="text-custom-green" /></span>
+                                <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Paid user</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100">$20 / user per month</span>
+                                <span className="inline-block text-center font-semibold mt-3 mb-3 text-indigo-100"> </span>
                             </div>
                             <div className="grid grid-cols-3 text-left">
                                 <span className="inline-block text-gray-500 text-left font-semibold mt-3 mb-3">Historical performance</span>
