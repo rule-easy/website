436cf98558820b30b107c27353356b9c8489a2ca
Prakash Shanbhag <praks.1529@gmail.com>
2024-02-27 19:00:03 +0530

    Remove the unused images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Uncomment below line for production build - Generates index.html
     // output: 'export',
     distDir: '_static',
     images: {
